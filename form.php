97dfda7baf87668d814a68cb73b2b9d313f126ae
Dylan Batista <115593733+Dylanbp7@users.noreply.github.com>
2022-11-13 17:40:32 -0300

    Primer commit

---
+++ form.php
@@ -0,0 +1,17 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] !="POST"){
+    header("Location: contacto.html");
+}
+
+$nombre = $_POST["nombre"];
+$apellido = $_POST ["apellido"];
+$email = $_POST ["email"];
+$telefono = $_POST ["telefono"];
+$asunto = $_POST ["asunto"];
+$mensaje = $_POST ["mensaje"];
+
+$rta = @mail("soydylanbatista@gmail.com", "Mensaje web: $asunto", $mensaje);
+var_dump($rta)
+
+
+?>
